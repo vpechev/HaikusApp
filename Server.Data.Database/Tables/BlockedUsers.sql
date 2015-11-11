e42bcae70664ebe9523e90c8560089899126336e
Krasimir Kostadinov <krasimir.kostadinov@scalefocus.com>
2015-11-11 10:08:45 +0200

    removed some unused files

--- Server.Data.Database/Tables/BlockedUsers.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[BlockedUsers]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-	[UserId] BIGINT NOT NULL,
-	[BlockedId] BIGINT NOT NULL, 
-    CONSTRAINT [FK_BlockedUsers_Users] FOREIGN KEY ([UserId]) REFERENCES [Users]([Id]),
-	CONSTRAINT [FK_BlockedUsers_Users2] FOREIGN KEY ([BlockedId]) REFERENCES [Users]([Id])
-)
